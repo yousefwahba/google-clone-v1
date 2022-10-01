c8458913505c52a8cf8b7fbcdb9c2cf7c4d6564d
yousefwahba <yousefwahba.official@gmail.com>
2022-10-01 19:03:05 +0200

    add google auth button

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header';
 import { getProviders, signIn } from 'next-auth/react';
+import GoogleButton from 'react-google-button';
 
 export default function signin({ providers }) {
   return (
@@ -9,18 +10,9 @@ export default function signin({ providers }) {
       <div className="mt-40">
         {Object.values(providers).map((provider) => (
           <div key={provider.name} className="flex flex-col items-center">
-            <img
-              className="w-52 object-cover"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
-              alt="google-logo"
-            />
-
-            <button
-              className="bg-blue-400 rounded-lg text-white p-3 hover:bg-blue-500 mt-10"
+            <GoogleButton
               onClick={() => signIn(provider.id, { callbackUrl: '/' })}
-            >
-              Sign in with {provider.name}
-            </button>
+            />
           </div>
         ))}
       </div>
